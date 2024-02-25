9c94aaf114c3ee373cb7353e120623ecc4d81090
anilfoss <anilfoss@gmail.com>
2024-02-26 02:44:07 +0530

    commit accordian code

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const Accordian = () => {
-    return <div>Accordian</div>;
+const Accordian = ({ title, body }) => {
+    const [accordianOpen, setAccordianOpen] = useState(false);
+
+    const refHeight = useRef(null);
+
+    const handleAccordian = () => {
+        setAccordianOpen(!accordianOpen);
+
+        if (!accordianOpen) {
+            refHeight.current.style.height =
+                refHeight.current.scrollHeight + "px";
+        } else {
+            refHeight.current.style.height = "0px";
+        }
+    };
+
+    return (
+        <>
+            <div className="bg-slate-200 inline-block mb-4 w-full rounded-md">
+                <button
+                    className="font-semibold text-left ps-4 pe-10 py-3 w-full relative"
+                    onClick={handleAccordian}
+                >
+                    {title}
+                    <span className="text-2xl px-3 absolute inset-y-0111 top-1/2 right-0 -translate-y-1/2">
+                        {accordianOpen ? "-" : "+"}
+                    </span>
+                </button>
+                <div
+                    ref={refHeight}
+                    className={`px-4 overflow-y-hidden transition-all ease-in-out duration-300 ${
+                        accordianOpen ? "mb-3" : "mb-0"
+                    }`}
+                    style={{ height: "0px" }}
+                >
+                    {body}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Accordian;
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Accordian from "../components/Accordian";
+
+const faqData = [
+    {
+        question: "What is Lorem Ipsum?",
+        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+        question: "Why do we use it?",
+        answer: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    },
+    {
+        question: "Where does it come from?",
+        answer: "Contrary to popular belief, Lorem Ipsum is not simply random text. Contrary to popular belief, Lorem Ipsum is not simply random text.",
+    },
+    {
+        question: "Is it readable English?",
+        answer: "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
+    },
+];
 
 const Faq = () => {
-    return <div>Faq</div>;
+    return (
+        <>
+            {faqData.map((item, index) => {
+                return (
+                    <Accordian
+                        key={index}
+                        title={item.question}
+                        body={item.answer}
+                    />
+                );
+            })}
+        </>
+    );
 };
 
 export default Faq;
